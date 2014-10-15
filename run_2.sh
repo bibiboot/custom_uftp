7d02df5e971397310684453cc10fef13037721e7
dan <rehman.d.fazlur@gmail.com>
2014-10-14 17:49:21 -0700

    Addin the correct run2

--- run_2.sh
+++ run_2.sh
@@ -8,8 +8,8 @@ sudo ./bin/a etc/data/data.bin 1:etc/data/recv.bin 2 > log_a_21.txt &
 # node2 sends file to node3 [ra23]
 sudo ./bin/a etc/data/data.bin 3:etc/data/recv.bin 2 > log_a_23.txt &
 
-# node2 recv file from node1 [rb21]
-sudo ./bin/b 2 1 > log_b_21.txt &
+# node2 recv file from node1 [rb12]
+sudo ./bin/b 2 1 > log_b_12.txt &
 
 # node2 recv file from node3 [rb32]
 sudo ./bin/b 2 3 > log_b_32.txt &
